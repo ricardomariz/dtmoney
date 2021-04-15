970f9070da3421875dc409deed0a9c89ecf9110e
Ricardo Mariz <shaaad@gmail.com>
2021-04-15 01:14:12 -0300

    created transaction hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
-import { FormEvent, useContext, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface ModalProps {
@@ -17,7 +17,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
 
   async function handleNewTransactionSubmit(event: FormEvent) {
     event.preventDefault()
