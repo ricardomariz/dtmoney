9e7d7b15a59febcc9e108a24f8f1d2807a54d166
Ricardo Mariz <shaaad@gmail.com>
2021-04-15 00:16:42 -0300

    created transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root')
 
@@ -20,13 +21,12 @@ function App() {
     setIsNewTransactionModalOpen(false)
   }
   return (
-    <>
-
+    <TransactionProvider>
       <GlobalStyle />
       <Header onOpenTransactionsModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
-    </>
+    </TransactionProvider>
   );
 }
 
---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "./services/api";
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  category: string;
+  type: string;
+  createdAt: string;
+}
+
+interface ProviderProp {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+export function TransactionProvider({ children }: ProviderProp) {
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
+
+  return (
+    <TransactionsContext.Provider value={transactions}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
